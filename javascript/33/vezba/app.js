d64b151df167c3320bc9c96705c1e850c02622ec
Maljevac Ajsa <ajsamaljevac265@gmail.com>
2025-08-21 15:50:59 +0200

    added a index.html file and a app.js file which is used for exercices

---
+++ javascript/33/vezba/app.js
@@ -0,0 +1,7 @@
+// 8. Čekanje u lancu
+
+// Napiši async funkciju delayedMessages koja:
+// prvo čeka 1 sekundu i ispisuje "Prva poruka",
+// zatim čeka još 2 sekunde i ispisuje "Druga poruka",
+// zatim čeka još 3 sekunde i ispisuje "Treća poruka".
+// 👉 Testiraj i proveri da li se poruke ispisuju sa pravim pauzama.
