630128a67e8cb42a2ae325ef3deb7a29a6019e2e
Maljevac Ajsa <ajsamaljevac265@gmail.com>
2025-08-21 16:00:46 +0200

    exercises

--- javascript/33/vezba/app.js
+++ javascript/33/vezba/app.js
@@ -5,3 +5,28 @@
 // zatim čeka još 2 sekunde i ispisuje "Druga poruka",
 // zatim čeka još 3 sekunde i ispisuje "Treća poruka".
 // 👉 Testiraj i proveri da li se poruke ispisuju sa pravim pauzama.
+
+// function delayedMessages() {
+//   setTimeout(() => console.log("Prva poruka"), 1000);
+//   setTimeout(() => console.log("Druga poruka"), 2000);
+//   setTimeout(() => console.log("Treca poruka"), 3000);
+// }
+
+// delayedMessages();
+
+function wait(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function delayedMessages() {
+  await wait(1000);
+  console.log("Prva poruka");
+
+  await wait(2000);
+  console.log("Druga poruka");
+
+  await wait(3000);
+  console.log("Treca poruka");
+}
+
+delayedMessages();
