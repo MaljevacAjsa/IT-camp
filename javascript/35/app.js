337620e42fef16624cabcbfc5f81aa3ef900732a
Maljevac Ajsa <ajsamaljevac265@gmail.com>
2025-08-30 15:28:56 +0200

    Add delayed callback execution and promise conversion examples

---
+++ javascript/35/app.js
@@ -0,0 +1,15 @@
+// 1. Delayed Callback Execution
+// Write a JavaScript function that takes a callback and invokes it after a delay of 2 second.
+// function invoked(func) {
+//   setTimeout(func, 2000);
+// }
+// invoked(() => {
+//   try {
+//     console.log(aki);
+//   } catch (e) {
+//     console.log(e.message);
+//   }
+// });
+
+// 2. Callback to Promise Conversion
+// Write a JavaScript program that converts a callback-based function to a Promise-based function.
